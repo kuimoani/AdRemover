8dce53f476c97317a6d670921d1f3526e250492f
Kiman Kim <kuimoani@gmail.com>
2019-08-12 14:47:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+참고: http://tech-blog.tomchambers.me/2016/01/13/How-to-write-a-simple-page-rewriting-Chrome-extension/
+참고: http://egloos.zum.com/Aimez/v/1870717
+참고: https://joshuajangblog.wordpress.com/2016/09/07/chrome-extension-content_scripts-tip/
+참고: https://stackoverflow.com/questions/13917047/how-to-get-a-content-script-to-load-after-a-pages-javascript-has-executed
+참고: https://developer.chrome.com/extensions/content_scripts
+
+
---
+++ injected.js
@@ -0,0 +1,46 @@
+﻿console.log("===== Youtube Ad Remover is started. =====");
+
+function removeAds() {
+	//close ad movie
+	if(document.getElementsByClassName("ytp-ad-skip-button").length > 0){
+		document.getElementsByClassName("ytp-ad-skip-button")[0].click();
+		console.log("Removed ad movie!!");
+	}
+	
+	//close ad banner
+	if(document.getElementsByClassName("ytp-ad-overlay-close-button").length > 0){
+		document.getElementsByClassName("ytp-ad-overlay-close-button")[0].click();
+		console.log("Removed ad banner!!");
+	}
+}
+
+window.addEventListener("load", function(){
+	console.log("window.addEventListener load");
+	//=============== show caption working well
+	//position: absolute; left:0px; top:0px; border: 2px solid red;color:red;z-index:9990;
+	var ele = document.createElement("div");
+	ele.style.position = "fixed";
+	ele.style.border = "solid 2px red";
+	ele.style.color = "red";
+	ele.style.zIndex = "9999";
+	var node = document.createTextNode("Youtube Ad Remover is working.");
+	ele.appendChild(node);
+	document.body.appendChild(ele);
+
+	//==============
+	var eleApp = document.getElementsByTagName("ytd-app")[0];
+	eleApp.addEventListener("DOMSubtreeModified", function(){
+		console.log("ytd-app DOMSubtreeModified");
+		
+		//=============== remove ads
+		var ele = document.getElementsByClassName("video-ads ytp-ad-module");
+		if(ele.length == 0) {
+			console.error("Cannot find ad element!.");
+			return;
+		}
+		
+		//DOMSubtreeModified, DOMNodeRemoved
+		ele[0].removeEventListener("DOMNodeInserted", removeAds);
+		ele[0].addEventListener("DOMNodeInserted", removeAds);
+	});
+});
